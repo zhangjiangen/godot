276625ee3cebd2cd6f397f1e9e414851590e3961
zhangjiangen <zhangjiangen@foxmail.com>
2022-05-27 20:39:50 +0800

    1

--- core/os/memory.cpp
+++ core/os/memory.cpp
@@ -787,7 +787,6 @@ private:
 			_memory_info;
 	MemoryHashMap<void *, MemoryDebugInfo> point_map;
 	MemoryHashMap<void *, int> point_size_map;
-	int TestCount = 0;
 	::Mutex _memory_info_mutex;
 
 	SmallMemoryManager() {
